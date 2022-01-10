30cf649603a7ca8c239ecbfa121b1610dea28f3a
András Ádám <adm.andras@gmail.com>
2022-01-10 16:01:30 +0200

    Mint and burn tokens

--- contracts/TestToken.sol
+++ contracts/TestToken.sol
@@ -2,21 +2,22 @@ pragma solidity ^0.8.2;
 
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/IERC20Metadata.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 
 // Custom ERC-20 implementation
-contract TestToken is IERC20, IERC20Metadata {
+contract TestToken is IERC20, IERC20Metadata, Ownable {
 
   // The name of the token
-  string private _name;
+  string private _name = "TestToken";
 
   // The symbol of the token
-  string private _symbol;
+  string private _symbol = "TST";
 
   // The number of decimals the token uses
   uint8 private _decimals = 18;
 
   // The total amount of tokens available
-  uint256 private _totalSupply;
+  uint256 private _totalSupply = 0;
 
   // Mapping balances to account addresses
   mapping (address => uint256) _balances;
@@ -29,13 +30,6 @@ contract TestToken is IERC20, IERC20Metadata {
 
   // Event fired when an account has approved another account to spend tokens on its behalf
   event Approval(address indexed owner, address indexed spender, uint256 value);
-
-  // Initialize the contract, set the name, symbol and total supply of the token
-  constructor(string memory name, string memory symbol, uint256 totalSupply) {
-    _name = name;
-    _symbol = symbol;
-    _totalSupply = _totalSupply;
-  }
 
   // Get the name of the token
   function name() external view returns (string memory) {
@@ -88,21 +82,38 @@ contract TestToken is IERC20, IERC20Metadata {
   }
 
   // Internal function to perform approvals
-  function _approve(address owner, address spender, uint256 value) private {
+  function _approve(address owner, address spender, uint256 amount) private {
     require(owner != address(0), "Cannot approve allowance from the zero address.");
     require(spender != address(0), "Cannot approve allowance to the zero address.");
-    _allowances[owner][spender] = value;
-    emit Approval(owner, spender, value);
+    _allowances[owner][spender] = amount;
+    emit Approval(owner, spender, amount);
   }
 
   // Internal function to perform transfers
-  function _transfer(address from, address to, uint256 value) private {
+  function _transfer(address from, address to, uint256 amount) private {
     require(from != address(0), "Cannot transfer from the zero address.");
     require(to != address(0), "Cannot transfer to the zero address.");
-    require(_balances[from] >= value, "Cannot transfer due to insufficient balance.");
-    _balances[from] -= value;
-    _balances[to] += value;
-    emit Transfer(from, to, value);
+    require(_balances[from] >= amount, "Cannot transfer due to insufficient balance.");
+    _balances[from] -= amount;
+    _balances[to] += amount;
+    emit Transfer(from, to, amount);
+  }
+
+  // Mint `amount` of new tokens to address `to`
+  function mint(address to, uint256 amount) public onlyOwner() {
+    require(to != address(0), "Cannot mint new tokens to the zero address.");
+    _totalSupply += amount;
+    _balances[to] += amount;
+    emit Transfer(address(0), to, amount);
+  }
+
+  // Burn `amount` of tokens from the address `from`
+  function burn(address from, uint256 amount) public onlyOwner() {
+    require(from != address(0), "Cannot burn tokens from the zero address.");
+    require(_balances[from] >= amount, "Cannot burn tokens due to insufficient balance.");
+    _totalSupply -= amount;
+    _balances[from] -= amount;
+    emit Transfer(from, address(0), amount);
   }
 
 }
