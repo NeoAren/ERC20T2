7be073e41fd237f47e6274b1ddbade49033abdfd
András Ádám <adm.andras@gmail.com>
2022-01-12 18:00:16 +0200

    Access contract symbol

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,26 +1,28 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect } from 'react';
+import { ethers } from 'ethers';
+import styles from './App.module.scss';
 
-function App() {
+import abi from './ERC20T2.json';
+
+const contractAddress = process.env.REACT_APP_CONTRACT_ADDRESS as string;
+
+const App = () => {
+
+  useEffect(() => {
+    (async () => {
+
+      const ethersProvider = new ethers.providers.Web3Provider(window.ethereum, 'any');
+      const ERC20T2 = new ethers.Contract(contractAddress, abi.abi, ethersProvider.getSigner());
+
+      const result = await ERC20T2.symbol();
+      console.log(result);
+
+    })();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div className={styles.App}>Hello there</div>
   );
-}
+};
 
 export default App;
